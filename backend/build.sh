c3a4e4755c5e577b05f29432ea8eec839b50aaff
ASHWANIGUPTA18 <ashwanig2004@gmail.com>
2026-05-25 22:34:53 +0530

    mm

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
+
+# Build frontend
+cd ../frontend
+npm install
+npm run build
+cd ../backend
+
+# Backend setup
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
